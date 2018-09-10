4a9b0a1f74a07b6fd4625f6ef266b11c8c3a1e07
mainidear <715699732@qq.com>
2018-09-10 09:25:57 +0800

    Initial commit

---
+++ src/test.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ucs_liaowenhua
+-- Date: 2018/9/7
+-- Time: 16:20
+-- To change this template use File | Settings | File Templates.
+--
+
+local function main()
+    ngx.say("hello world, i am a test")
+end
+main()
